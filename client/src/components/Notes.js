002d1296d25f01292e38103e5a631d043d510fb2
Jobin John K <jobinjohnk5@gmail.com>
2022-08-09 18:18:23 +0530

    add color to note text and delete icon

--- client/src/components/Notes.js
+++ client/src/components/Notes.js
@@ -4,37 +4,44 @@ import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import "./Notes.css";
+import DeleteIcon from "@mui/icons-material/Delete";
+import IconButton from "@mui/material/IconButton";
 
 export default function Notes() {
   return (
     <Grid className="grid" container spacing={2} justifyContent="center">
       <Grid item>
         <Card sx={{ width: 350 }}>
-          <CardContent>
+          <CardContent className="cardNote">
             <h4>Hello</h4>
             <p>this is content</p>
           </CardContent>
-          <CardActions></CardActions>
+          <CardActions>
+            <IconButton color="primary">
+              <DeleteIcon />
+            </IconButton>
+          </CardActions>
         </Card>
       </Grid>
       <Grid item>
         <Card sx={{ width: 350 }}>
-          <CardContent>
+          <CardContent className="cardNote">
             <h4>Hello</h4>
-            <p>this is content
-            this is content
-            this is content
-            this is content
-            this is content
-            this is content
-            this is content</p>
+            <p>
+              this is content this is content this is content this is content
+              this is content this is content this is content
+            </p>
           </CardContent>
-          <CardActions></CardActions>
+          <CardActions>
+            <IconButton color="primary">
+              <DeleteIcon />
+            </IconButton>
+          </CardActions>
         </Card>
       </Grid>
       <Grid item>
         <Card sx={{ width: 350 }}>
-          <CardContent>
+          <CardContent className="cardNote">
             <h4>Hello</h4>
             <p>this is content</p>
           </CardContent>
